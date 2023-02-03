0e1e8f4a2f1caca863c351f02fd38442c5771a30
Melley Gebretatios <gmelley4@gmail.com>
2023-02-02 19:51:46 -0500

    Edit App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 // import logo from './logo.svg';
 // import "./App.css";
 // import { React, useEffect, useState } from "react";
-import axios from 'axios';
+import axios from "axios";
 
 function App() {
   axios
     .get("http://127.0.0.1:5000/toys")
-    .then(() => {
-      console.log("response");
+    .then((response) => {
+      console.log(response);
     })
     .catch(() => {
       console.log("error");
