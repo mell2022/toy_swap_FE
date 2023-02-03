7c48c51e420b5f37661e209c6b6b4beaac6f0d39
Melley Gebretatios <gmelley4@gmail.com>
2023-02-02 19:09:33 -0500

    Add axios and create first  component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import "./App.css";
+// import { React, useEffect, useState } from "react";
+import axios from 'axios';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  axios
+    .get("http://127.0.0.1:5000/toys")
+    .then(() => {
+      console.log("response");
+    })
+    .catch(() => {
+      console.log("error");
+    });
 }
 
 export default App;
